b141598821e2bdbedffdef7e7293e25b08c84318
WF12345 <46488554+WF12345@users.noreply.github.com>
2021-05-21 17:15:01 +0800

    谷粒商城-创建后台工程

---
+++ wms/src/test/java/com/atguigu/wms/WmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
